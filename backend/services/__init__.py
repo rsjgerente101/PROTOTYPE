b421a686a736e0f6375524ff9fda6870314bc861
Risa Joy Gerente <risajoygerente@gmail.com>
2026-05-20 14:10:13 +0800

    Add services package initialization with service module exports

---
+++ backend/services/__init__.py
@@ -0,0 +1,9 @@
+"""Services package for backend service modules."""
+
+__all__ = [
+    "add_customer_service",
+    "enhancement_service",
+    "metrics_service",
+    "routing_service",
+    "osm_service",
+]
