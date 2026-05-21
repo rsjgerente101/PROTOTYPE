0e1cae548a35dcc0bf5866a2d9fc0f55b1b3bc9b
Risa Joy Gerente <risajoygerente@gmail.com>
2026-05-21 09:18:21 +0800

    Remove refactored files related to Amazon issue limiting each rep to 3 CAP customers

--- backend/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Services package for backend service modules."""
-
-__all__ = [
-    "add_customer_service",
-    "enhancement_service",
-    "metrics_service",
-    "routing_service",
-    "osm_service",
-]
